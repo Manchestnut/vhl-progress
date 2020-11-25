c5d9970a7b3e5aca08e42c20afcab061f005f6e3
Luke Flores <luke.manongsong@gmail.com>
2020-11-25 17:20:50 +0800

    update 8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var num = 0;
 var numTotal = 0;
 var maxTotal = 0;
 var max = 0;
-console.log(items.length);
+console.log("Item Quantity: "+items.length);
 for(i=0;i<items.length;i++){
     num = parseInt(items[i].value);
     max = parseInt(items[i].max);
@@ -17,4 +17,4 @@ var percent = (numTotal * 100) / maxTotal;
 var rounded = percent.toFixed(2);
 bar.style.width = percent+"%";
 indicator.textContent = rounded+"%";
-console.log(rounded);
+console.log("Total Percentage: "+rounded+"%");
